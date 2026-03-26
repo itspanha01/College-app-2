c936aa709d0ff3f03ae07300c6f0e70a977a2bce
Lymiy Chrean <lymiychrean@Lymiys-MacBook-Pro.local>
2026-03-26 12:17:57 +0700

    test

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from modulefinder import test
 import os
 import sys
 
@@ -20,3 +21,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# test
+# test
+# test
